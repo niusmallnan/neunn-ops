e9688a2c5b05dc1ce3fb1710d9d2afefe0bbd800
zhangzb <zhangzb@neunn.com>
2014-01-08 16:42:28 +0800

    查看网卡上某一端口的数据包

---
+++ system/commandlinefu.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# commandlinefu.sh
+# Copyright (C) 2014 zhangzb <zhangzb@neunn.com>
+#
+# Distributed under terms of the MIT license.
+#
+# 收集各种简洁牛掰的小脚本
+#
+
+
+#监控eth0上80端口的数据包
+tcpdump -i eth0 port 80 -w -
+
+
+
