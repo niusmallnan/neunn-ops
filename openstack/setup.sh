f0e65a0ac84d7c3197e9fd99bf464c686d18e26a
niusmallnan <zhangzb@neunn.com>
2014-06-16 11:19:39 +0800

    setup自动拷贝脚本到salt的执行目录

---
+++ openstack/setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+# setup.sh
+# Copyright (C) 2014 niusmallnan <zhangzhibo521@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+mkdir -p /srv/salt/openstack
+cp modify_* /srv/salt/openstack/
+cp nova_* /srv/salt/openstack/
+
