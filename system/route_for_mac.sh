f6fce3c65c94ee541ee046cfe1e7a3cf61cad4e1
niusmallnan <zhangzb@neunn.com>
2014-06-25 11:23:32 +0800

    修改默认路由表

--- system/route_for_mac.sh
+++ system/route_for_mac.sh
@@ -9,9 +9,13 @@
 
 route -n delete -net 172.17.0.0/16 172.16.99.1
 route -n delete -net 192.168.250.0/24 172.16.99.1
+route -n delete -net 192.168.251.0/24 172.16.99.1
 route -n delete -net 192.168.252.0/24 172.16.99.1
+route -n delete -net 192.168.253.0/24 172.16.99.1 
 
 route -n add -net 172.17.0.0/16 172.16.99.1
 route -n add -net 192.168.250.0/24 172.16.99.1
-route -n delete -net 192.168.252.0/24 172.16.99.1
+route -n add -net 192.168.251.0/24 172.16.99.1
+route -n add -net 192.168.252.0/24 172.16.99.1
+route -n add -net 192.168.253.0/24 172.16.99.1
 
